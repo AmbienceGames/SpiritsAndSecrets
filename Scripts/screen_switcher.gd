e9bc0ede6b2f84eb899d72615f1135d1c1539600
GEORGE MATTA <jojomedhat2004@gmail.com>
2024-07-17 17:00:28 -0700

    Fix screen order

--- Scripts/screen_switcher.gd
+++ Scripts/screen_switcher.gd
@@ -2,12 +2,33 @@ extends Node2D
 
 @export
 var tavern_screens: Array[Node2D] = []
+var current_screen = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	tavern_screens[0].visible = true
+	tavern_screens[current_screen].visible = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _cycle_right() -> void:
+	var next_screen = current_screen + 1
+	if next_screen > len(tavern_screens) - 1:
+		next_screen = 0
+		
+	tavern_screens[next_screen].visible = true
+	tavern_screens[current_screen].visible = false
+	current_screen = next_screen
+
+
+func _cycle_left() -> void:
+	var next_screen = current_screen - 1
+	if next_screen < 0:
+		next_screen = len(tavern_screens) - 1
+		
+	tavern_screens[next_screen].visible = true
+	tavern_screens[current_screen].visible = false
+	current_screen = next_screen
